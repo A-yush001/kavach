bb4b928095bd420dde20a202798c5d1946c3e9a7
Ayush Pandey <ayushji3sep@gamil.com>
2023-04-23 19:35:56 +0530

    new commit

--- kavach_project/settings.py
+++ kavach_project/settings.py
@@ -118,6 +118,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000'
+],
+ALLOWED_HOSTS = [
+    'localhost',
+],
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8000',
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
